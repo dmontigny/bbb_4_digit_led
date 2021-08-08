5d264e40eabafc9ba298717f166e3aed4861a9da
dmontigny <dmontigny27@gmail.com>
2021-08-08 17:24:17 -0400

    new initial commit

---
+++ TestProject/src/test1.c
@@ -0,0 +1,14 @@
+/*
+ * test1.c
+ *
+ *  Created on: Aug 8, 2021
+ *      Author: dmontylx
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("!!!Hello World!!!");
+	return EXIT_SUCCESS;
+}
